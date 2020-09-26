80bd174917ad94916dc59d28bdd385db7aa33c0b
Trang Doan <doantranghp2000@gmail.com>
2020-09-26 22:19:01 +0700

    Finalize basic version

---
+++ Flixapp/MovieCell.swift
@@ -0,0 +1,29 @@
+//
+//  MovieCell.swift
+//  Flixapp
+//
+//  Created by Trang Doan on 9/26/20.
+//  Copyright © 2020 Trang Doan. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
